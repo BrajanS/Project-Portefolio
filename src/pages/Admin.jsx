526221e72e2e5f60af9617a66c4f7933bcc1581f
BrajanS <szczapbrajan33@gmail.com>
2024-07-10 13:58:21 +0200

    1.17

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -12,12 +12,9 @@ export default function Admin() {
   const [password,setPassword] = useState('')
   const [isLogged,setIsLogged] = useState(false)
 
-  localStorage.setItem("USERNAME","admin")
-  localStorage.setItem("PASSWORD","admin")
-
   function handleSubmit(event){
     event.preventDefault()
-    if(username=== "admin" && password === "admin"){
+    if(username=== localStorage.getItem("USERNAME") && password === localStorage.getItem("PASSWORD")){
       setIsLogged(true)
     }else{
       alert('Login incorrecte')
